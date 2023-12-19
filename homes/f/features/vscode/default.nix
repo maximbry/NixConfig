92eb199db1f918b1ff26a2c1a921251fa6f5ebf0
maximbry <github@maximbry.anonaddy.com>
2023-12-19 21:00:10 +0300

    feat: update vscode settings

--- homes/f/features/vscode/default.nix
+++ homes/f/features/vscode/default.nix
@@ -4,5 +4,11 @@
     mutableExtensionsDir = true;
     enableExtensionUpdateCheck = true;
     enableUpdateCheck = false;
+    userSettings = {
+      "files.autoSave" = "onFocusChange";
+      "editor.tabSize" = 2;
+      "files.autoGuessEncoding" = true;
+      "editor.insertSpaces" = true;
+    };
   };
 }
