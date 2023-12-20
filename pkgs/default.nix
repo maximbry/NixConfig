84749839adb8ba61292459000821bae78a52c8be
maximbry <github@maximbry.anonaddy.com>
2023-12-20 16:57:08 +0300

    refactor: delete obsolete packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,2 @@
 { pkgs ? import <nixpkgs> { } }: rec {
-    tg-session-keeper = pkgs.python3Packages.callPackage ./tg-session-keeper { };
-    telegram-session-keeper = pkgs.callPackage ./telegram-session-keeper {};
 }
