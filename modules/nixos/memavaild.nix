edb1fa8c6ff0e1dc56c3bd4fc898f1e491c28800
maximbry <github@maximbry.anonaddy.com>
2023-12-20 22:39:29 +0300

    refactor: improve memavaild service

--- modules/nixos/memavaild.nix
+++ modules/nixos/memavaild.nix
@@ -5,22 +5,34 @@ with lib;
 let
   inherit (lib.types) attrsOf coercedTo listOf oneOf str int bool;
   cfg = config.services.memavaild;
+  confFile = pkgs.writeText "memavaild.conf" ''
+    ${cfg.extraConfig}
+  '';
 in {
 
   options.services.memavaild = {
     enable = mkEnableOption (lib.mdDoc "memavaild");
+
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+      description = lib.mdDoc ''
+        Extra configuration directives that should be added to
+        `prelockd.conf`
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
     users.users.memavaild = {
       description = "memavaild service user";
-      createHome = false;
       isSystemUser = true;
       group = "memavaild";
     };
     users.groups.memavaild = { };
     systemd.packages = [ pkgs.memavaild ];
     systemd.services.memavaild.wantedBy = [ "multi-user.target" ];
-    environment.etc."memavaild.conf".source = "${pkgs.memavaild}/etc/memavaild.conf";
+    systemd.services.memavaild.restartTriggers = [ confFile ];
+    environment.etc."memavaild.conf".source = confFile;
   };
 }
