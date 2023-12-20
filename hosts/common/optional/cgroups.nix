1fcfa92501688ae7ac35e3e3ce57b57eb9bb12c9
maximbry <github@maximbry.anonaddy.com>
2023-12-20 22:23:35 +0300

    chore: add boot cgroup param

--- hosts/common/optional/cgroups.nix
+++ hosts/common/optional/cgroups.nix
@@ -3,4 +3,6 @@
     enableUnifiedCgroupHierarchy = lib.mkForce true;
     enableCgroupAccounting = lib.mkForce true;
   };
+
+  boot = { kernelParams = [ "systemd.unified_cgroup_hierarchy=1" ]; };
 }
