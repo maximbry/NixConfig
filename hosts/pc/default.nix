cab933489b87bccb967dc7502d6cb8c406ede3ce
maximbry <github@maximbry.anonaddy.com>
2024-01-23 13:36:23 +0300

    feat: return some of the risky things

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -22,6 +22,9 @@
     ../common/optional/cgroups.nix
     ../common/optional/faster-shutdown.nix
     ../common/optional/plymouth.nix
+    ../common/optional/uresourced.nix
+    ../common/optional/prelockd.nix
+    ../common/optional/memavaild.nix
   ];
 
   # temporary
