c375cfa12dd67a5239e50ef0d828c66bf1e32a7f
maximbry <github@maximbry.anonaddy.com>
2024-01-24 20:04:02 +0300

    feat: add headless package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,4 +5,13 @@
   zapret = pkgs.callPackage ./zapret { };
   nekoray = pkgs.callPackage ./nekoray { };
   doom2d-forever = pkgs.callPackage ./doom2d-forever { };
+  doom2d-forever-headless = pkgs.callPackage ./doom2d-forever {
+    headless = true;
+    withOpenAL = false;
+    withSDL2_mixer = true;
+    withSDL2 = true;
+    disableGraphics = true;
+    withOpenGL2 = false;
+    withHolmes = true;
+  };
 }
