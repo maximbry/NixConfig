99cc7718edd6907af0996a2f6a99b55c0f9690b3
maximbry <github@maximbry.anonaddy.com>
2024-01-29 17:29:07 +0300

    feat: update chromium config

--- homes/f/features/net/browser/chromium/default.nix
+++ homes/f/features/net/browser/chromium/default.nix
@@ -1,5 +1,29 @@
 { pkgs, config, ... }: {
-  programs.chromium = {
+  programs.chromium = let
+    flagSwitches = [
+      "--ignore-gpu-blocklist"
+      "--enable-gpu-rasterization"
+      "--enable-native-gpu-memory-buffers"
+      "--enable-accelerated-2d-canvas"
+      "--enable-oop-rasterization"
+      "--enable-zero-copy"
+      "--disable-beforeunload"
+      "--enable-quic"
+      "--extension-mime-request-handling=always-prompt-for-install"
+      "--fingerprinting-canvas-image-data-noise"
+      "--fingerprinting-canvas-measuretext-noise"
+      "--fingerprinting-client-rects-noise"
+      "--force-punycode-hostnames"
+      "--max-connections-per-host=15"
+      "--no-pings"
+      "--show-avatar-button=always"
+      "--enable-smooth-scrolling"
+      "--enable-features=MinimalReferrers,NoCrossOriginReferrers,SetIpv6ProbeFalse,WebRTCPipeWireCapturer,WebRtcHideLocalIpsWithMdns"
+      "--ozone-platform-hint=auto"
+    ];
+  in rec {
     enable = true;
+    package = pkgs.ungoogled-chromium;
+    commandLineArgs = flagSwitches;
   };
 }
