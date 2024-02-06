cab71390c7778349b5b9655017a09a465a10989e
maximbry <github@maximbry.anonaddy.com>
2024-02-06 20:21:52 +0300

    feat: add new package

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,5 +12,30 @@
   };
 
   additions = final: _prev: import ../pkgs { pkgs = final; };
-  modifications = final: prev: { };
+  modifications = final: prev: {
+    fpc-git = prev.fpc.overrideAttrs (oldAttrs: {
+      src = final.fetchgit {
+        url = "https://gitlab.com/freepascal.org/fpc/source.git";
+        rev = "46508f6af16b7f676ca05bc9f84f904d3c2aac23";
+        sha256 = "sha256-0HjI4FXWXA8P468dK7GLSofgDdPfCSvyohJlIbS/KSc=";
+      };
+      patches = [ ./0001-Mark-paths-for-NixOS.patch ];
+
+      postPatch = ''
+        # substitute the markers set by the mark-paths patch
+        substituteInPlace compiler/systems/t_linux.pas --subst-var-by dynlinker-prefix "${final.glibc}"
+        substituteInPlace compiler/systems/t_linux.pas --subst-var-by syslibpath "${final.glibc}/lib"
+        # Replace the `codesign --remove-signature` command with a custom script, since `codesign` is not available
+        # in nixpkgs
+        # Remove the -no_uuid strip flag which does not work on llvm-strip, only
+        # Apple strip.
+        substituteInPlace compiler/Makefile \
+          --replace \
+            "\$(CODESIGN) --remove-signature" \
+            "${prev.fpc}/remove-signature.sh}" \
+          --replace "ifneq (\$(CODESIGN),)" "ifeq (\$(OS_TARGET), darwin)" \
+          --replace "-no_uuid" ""
+      '';
+    });
+  };
 }
