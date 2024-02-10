375ef43b7e95798573f491d2ed3f12e6ea82994f
maximbry <github@maximbry.anonaddy.com>
2024-02-10 15:51:57 +0300

    feat: start kwallet on startup

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -31,6 +31,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  security.pam.services.login.enableKwallet = true;
 
   services.ivpn = { enable = true; };
   programs.adb.enable = true;
